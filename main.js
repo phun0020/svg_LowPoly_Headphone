cce6cd66fb818a8a13f9a4a681ac349c85b724ff
phun0020 <phun0020@algonquinlive.com>
2017-04-21 19:55:49 -0400

    add audio and animation

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var headphone = document.getElementById('headphone');
-headphone.addEventListener('load', function() {
+headphone.addEventListener('load', () => {
     var svgDoc = headphone.contentDocument;
     var outlineHeadphone1 = svgDoc.getElementById('outline-headphone1');
     var outlineHeadphone2 = svgDoc.getElementById('outline-headphone2');
@@ -8,12 +8,31 @@ headphone.addEventListener('load', function() {
     var iconShape = svgDoc.getElementById('icon');
     var heartDetail = svgDoc.getElementById('detail-heart');
 
-    //animate selector
+    // animate selector
     var animateToHeart = svgDoc.getElementById('animation-to-heart');
     var animateToPlay = svgDoc.getElementById('animation-to-play');
 
+    // audio
+    var audio = new Audio('audio/Cold_Heart_Hoaprox_cut.mp3');
+    if (typeof audio.loop == 'boolean'){ audio.loop = true; }
+
+    // remove and pause
+    var rmAndPause = () => {
+        iconShape.classList.remove('heart-beat');
+        heartDetail.classList.remove('fade-in');
+        audio.pause();
+    };
+
+    var addAndPlay = () => {
+        iconShape.classList.add('heart-beat');
+        heartDetail.classList.add('fade-in');
+        audio.play();
+    };
+
+    // -------------------------
     // Icon click event
-    svgDoc.addEventListener('click', function() {
+    // -------------------------
+    svgDoc.addEventListener('click', () => {
         // change color inside background
         insideGradient.classList.toggle('change-color-inside-background');
 
@@ -21,23 +40,55 @@ headphone.addEventListener('load', function() {
         // animate to heart <-> play
         // -------------------------
         if(iconShape.classList.contains('toPlay')) {
+            audio.currentTime = 0;
+            if(iconShape.classList.contains('heart-beat')) {
+                rmAndPause();
+            }
             iconShape.classList.remove('toPlay');
             animateToPlay.beginElement();
         }
         else {
             iconShape.classList.add('toPlay');
+            if(!iconShape.classList.contains('heart-beat')) {
+                addAndPlay();
+            }
             animateToHeart.beginElement();
         }
-
-        iconShape.classList.toggle('heart-beat');
-        heartDetail.classList.toggle('fade-in'); 
         
         // shaking outline
         outlineHeadphone1.classList.toggle('shake-outline');
-        setTimeout(function() { 
+        setTimeout(() => { 
             outlineHeadphone2.classList.toggle('shake-outline');     
-        }, 250);    
+        }, 250);
     }, false);
 
+    // -------------------------
+    // Icon hover event
+    // -------------------------
+    svgDoc.addEventListener('mouseover', (e) => {
+        if(iconShape.classList.contains('toPlay')) {
+            if(!iconShape.classList.contains('heart-beat')) {
+                addAndPlay();
+            }
+            audio.play();
+        }
+        else {
+            audio.pause();
+        }
+    }, false)
+
+    svgDoc.addEventListener('mouseout', (e) => {
+        if(iconShape.classList.contains('heart-beat')) {
+            rmAndPause();            
+        }
+    }, false);
+
+    // -------------------------
+    // loop & replay
+    // -------------------------
+    audio.addEventListener('ended', () => {
+        audio.currentTime = 0;
+        audio.play();
+    }, false);
 
 }, false);
