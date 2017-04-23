f70d6bd2e725e3ddd2ca5cde3404a70e3d5f7eed
phun0020 <phun0020@algonquinlive.com>
2017-04-22 20:07:42 -0400

    add start functions
    maybe for not connected state

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
 var headphone = document.getElementById('headphone');
+var startBtn = document.getElementById('startBtn');
 headphone.addEventListener('load', () => {
     var svgDoc = headphone.contentDocument;
     var outlineHeadphone1 = svgDoc.getElementById('outline-headphone1');
     var outlineHeadphone2 = svgDoc.getElementById('outline-headphone2');
-
-    var insideGradient = svgDoc.getElementById('inside-gradient-stop1');
+    var iconContainer = svgDoc.getElementById('inside-background-rectangle');
     var iconShape = svgDoc.getElementById('icon');
     var heartDetail = svgDoc.getElementById('detail-heart');
+
+    // magic begins
+
+    var doSetTimeOut = (polygonEls, polygonLength, i, oldClass, newClass) => {
+        setInterval(() => {
+            polygonEls[i].classList.remove(oldClass);
+            polygonEls[i].classList.remove('st420');
+            polygonEls[i].classList.add(newClass);
+            
+            polygonEls[polygonLength + i].classList.remove(oldClass);
+            polygonEls[polygonLength + i].classList.remove('st420');
+            polygonEls[polygonLength + i].classList.add(newClass);
+        }, 8 * i);
+    };
+
+    startBtn.addEventListener('click', () => {
+        var polygonEls = svgDoc.getElementById('gHeadPhone').children;
+        var polygonLength = polygonEls.length;
+        var oldClass = "";
+        var newClass = "";
+
+        for(var i = 0; i < polygonLength/2; i++) {
+            oldClass = polygonEls[i].classList[0];
+            newClass = oldClass.replace('r', '');
+            doSetTimeOut(polygonEls, polygonLength/2, i, oldClass, newClass);
+        }
+    });
 
     // animate selector
     var animateToHeart = svgDoc.getElementById('animation-to-heart');
@@ -14,27 +41,34 @@ headphone.addEventListener('load', () => {
 
     // audio
     var audio = new Audio('audio/Cold_Heart_Hoaprox_cut.mp3');
-    if (typeof audio.loop == 'boolean'){ audio.loop = true; }
+    if (typeof audio.loop == 'boolean') { audio.loop = true; }
 
     // remove and pause
     var rmAndPause = () => {
         iconShape.classList.remove('heart-beat');
         heartDetail.classList.remove('fade-in');
         audio.pause();
+        // remove shaking outline
+        outlineHeadphone1.classList.remove('shake-outline');
+        outlineHeadphone2.classList.remove('shake-outline2');
     };
 
     var addAndPlay = () => {
         iconShape.classList.add('heart-beat');
         heartDetail.classList.add('fade-in');
         audio.play();
+        // shaking outline
+        outlineHeadphone1.classList.add('shake-outline');
+        outlineHeadphone2.classList.add('shake-outline2');     
     };
 
     // -------------------------
     // Icon click event
     // -------------------------
-    svgDoc.addEventListener('click', () => {
+    iconContainer.addEventListener('click', () => {
         // change color inside background
-        insideGradient.classList.toggle('change-color-inside-background');
+        iconContainer.classList.toggle('change-color-inside-background');
+        iconContainer.classList.toggle('fill-gradient');
 
         // -------------------------
         // animate to heart <-> play
@@ -55,15 +89,10 @@ headphone.addEventListener('load', () => {
             animateToHeart.beginElement();
         }
         
-        // shaking outline
-        outlineHeadphone1.classList.toggle('shake-outline');
-        setTimeout(() => { 
-            outlineHeadphone2.classList.toggle('shake-outline');     
-        }, 250);
-    }, false);
+    }, false);   
 
     // -------------------------
-    // Icon hover event
+    // Doc hover event
     // -------------------------
     svgDoc.addEventListener('mouseover', (e) => {
         if(iconShape.classList.contains('toPlay')) {
@@ -84,7 +113,7 @@ headphone.addEventListener('load', () => {
     }, false);
 
     // -------------------------
-    // loop & replay
+    // Loop & replay
     // -------------------------
     audio.addEventListener('ended', () => {
         audio.currentTime = 0;
